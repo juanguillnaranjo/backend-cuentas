4c43fc5b9b7f86d5ef8fbbb5caddaab7b8967929
juanguillnaranjo <juanguill.naranjo@gmail.com>
2026-03-04 00:26:10 -0500

    Update app.js

--- app.js
+++ app.js
@@ -15,15 +15,6 @@ app.get('/', (req, res) => {
   res.send('Servidor activo ojala!');
 });
 
-mongoose.connect(process.env.MONGODB_URI)
-  .then(() => console.log('MongoDB conectado, eso creo'))
-  .catch(err => console.error('MongoDB Atlas error:', err));
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Backend corriendo en puerto ${PORT}`);
-});
-
 // cargar rutas
 var cuentas_routes = require('./routes/routes-cuentas');
 
