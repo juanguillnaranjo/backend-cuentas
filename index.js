ee23d1456dd3cb7b4bf6ddcbdf5c024d9d292c80
juanguillnaranjo <juanguill.naranjo@gmail.com>
2026-03-04 00:37:12 -0500

    Update index.js

--- index.js
+++ index.js
@@ -22,14 +22,6 @@ mongoose.connect('process.env.MONGODB_URI')
 
 const mysql = require('mysql2');
 
-mongoose.connect(process.env.MONGODB_URI)
-  .then(() => console.log('MongoDB conectado, eso creo'))
-  .catch(err => console.error('MongoDB Atlas error:', err));
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Backend corriendo en puerto ${PORT}`);
-});
 
 // Crear conexión
 const connection = mysql.createConnection({
