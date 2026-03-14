1a4e1e27ab402a2a6e6f7e89b8565e8419ca10c7
juanguillnaranjo <juanguill.naranjo@gmail.com>
2026-03-14 00:17:46 -0400

    Add dynamic saldo2 calculation to movimiento object

--- controllers/controler-libro-general.js
+++ controllers/controler-libro-general.js
@@ -64,7 +64,7 @@ var controller = {
 
 		        return {
 		          ...mov.toObject(),
-		          saldo: saldoAcumulado  // 👈 saldo calculado dinámicamente
+		          saldo2: saldoAcumulado  // 👈 saldo calculado dinámicamente
 		        };
 
 	    	});
